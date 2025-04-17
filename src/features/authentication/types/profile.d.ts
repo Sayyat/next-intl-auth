51fdaaf39d906a711946923d44dc9999814b8788
sayat <sayatraykulov@gmai.com>
2025-04-17 11:36:46 +0500

    refactor: update IProfile

--- src/features/authentication/types/profile.d.ts
+++ src/features/authentication/types/profile.d.ts
@@ -1,14 +1,18 @@
 import {ICity} from "@/features/common/city"
 
 export interface IProfile {
-    email: string,
     firstname: string,
     lastname: string,
-    phone_number: string,
-    organization_name: string,
-    city: ICity,
-    image_url: string,
-    is_superuser: boolean,
+    middle_name: string,
+    birthdate: string,
+    iin: string,
+    phone_number: ICity,
+    email: string,
+    image: boolean,
+    city: string,
+    address: string,
+    is_active: string,
+    is_staff: string,
     created_at: string,
     updated_at: string,
 }
