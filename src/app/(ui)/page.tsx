cfac20cae97893e3fe098cef4ea309c03dd43321
sayat <sayatraykulov@gmai.com>
2025-04-17 14:29:22 +0500

    refactor: update page

--- src/app/(ui)/page.tsx
+++ src/app/(ui)/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   const { data } = useProfile();
   const t = useTranslations("app.(ui).page");
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col items-center justify-center w-full">
       <span>{t("welcomeMessage", { username: data?.firstname })}</span>
     </div>
   );
