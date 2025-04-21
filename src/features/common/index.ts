b5554ebb1277b07f1c5250be6280dc213a3b7661
sayat <sayatraykulov@gmai.com>
2025-04-21 08:46:43 +0500

    refactor: remove extra files

--- src/features/common/index.ts
+++
@@ -1,5 +0,0 @@
-export type {TActivity, IActivityOption} from "./types/api";
-export type {ICity} from "./types/city"
-export {createCommonStateSchema} from "./lib/zod"
-export {useCommonStore} from "./store/useCommonStore"
-export {useCommonValidation} from "./hooks/useCommonValidation"
